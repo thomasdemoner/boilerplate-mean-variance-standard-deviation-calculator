d5ea7c651cee81edadb5954b0f5214ed92aef8fc
Thomas Demoner <46510558+thomasdemoner@users.noreply.github.com>
2021-12-23 00:17:18 +0000

    copy of fcc submit

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,22 @@ import numpy as np
 
 def calculate(list):
 
+  # print(len(list))
+  try:
+    len(list) == 9
+  except:
+    print("List must contain nine numbers.")
 
+  num_arr= np.asarray(list)
+  num_arr = np.reshape(num_arr, (3, 3))
 
+  ret_dict = {
+    'mean': [np.mean(num_arr, axis= 0).tolist(), np.mean(num_arr, axis= 1).tolist(), np.mean(num_arr)],
+    'variance': [np.var(num_arr, axis= 0).tolist(), np.var(num_arr, axis= 1).tolist(), np.var(num_arr)],
+    'standard deviation': [np.std(num_arr, axis= 0).tolist(), np.std(num_arr, axis= 1).tolist(), np.std(num_arr)],
+    'max': [np.max(num_arr, axis= 0).tolist(), np.max(num_arr, axis= 1).tolist(), np.max(num_arr)],
+    'min': [np.min(num_arr, axis= 0).tolist(), np.min(num_arr, axis= 1).tolist(), np.min(num_arr)],
+    'sum': [np.sum(num_arr, axis= 0).tolist(), np.sum(num_arr, axis= 1).tolist(), np.sum(num_arr)],
+  }
 
-    return calculations
+  return ret_dict
